0ddbe689902c2dbbe3e3167b3bb72b66a25a0aa8
Roland Rytz <roland.rytz@gmail.com>
2016-07-22 23:35:22 +0200

    Implement Conway's Game of Life in WebGL

---
+++ fragmentshader.js
@@ -0,0 +1,109 @@
+var fragmentShader = `
+
+precision mediump float;
+
+uniform sampler2D u_image;
+uniform vec2 textureSize;
+
+varying vec2 texCoord;
+
+uniform vec2 onePixel;
+
+vec2 getCoords(vec2 coord, vec2 offset){
+	//return vec2(mod(coord.x + onePixel.x * offset.x, 1.0), mod(coord.y + onePixel.y * offset.y, 1.0));
+	return mod(coord + onePixel * offset, 1.0);
+}
+
+float r = 0.0;
+float sum = 0.0;
+
+void main(void){
+
+	float r = texture2D(u_image, texCoord).r;
+
+	float sum = 0.0;
+
+	sum += texture2D(u_image, getCoords(texCoord, vec2(-1.0, -1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(0.0, -1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(1.0, -1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(-1.0, 1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(0.0, 1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(1.0, 1.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(-1.0, 0.0))).r;
+	sum += texture2D(u_image, getCoords(texCoord, vec2(1.0, 0.0))).r;
+
+	//sum = sum / 255.0;
+
+	/*float g = 0.0;
+	float b = texture2D(u_image, texCoord).b * 0.95;
+	
+	if(b > 0.2){
+		g = texture2D(u_image, texCoord).g * 1.06;
+	} else {
+		g = texture2D(u_image, texCoord).g * 0.98;
+	}
+
+	if(r != 0.0 && (sum < 2 || sum > 3)){
+		r = 0.0;
+		g = 0.1;
+		b = 1.0;
+	} else if(r == 0.0 && sum == 3){
+		r = 1.0;
+		g = 1.0;
+		b = 1.0;
+	} else {
+		
+	}
+	gl_FragColor = vec4(r, g, b, 1.0);*/
+	
+
+	if(r != 0.0 && (sum < 2.0 || sum > 3.0)){
+		r = 0.0;
+	} else if(r == 0.0 && sum == 3.0){
+		r = 1.0;
+	}
+	
+	gl_FragColor = vec4(r, r, r, 1.0);
+}
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ init.js
@@ -0,0 +1,55 @@
+
+
+
+var webglife = new Webglife();
+
+function Webglife(){
+
+	var canvasContainer = document.getElementById("canvasContainer");
+	var canvas = document.getElementById("renderCanvas");
+
+	function scale(){
+
+		canvas.width = ~~(canvasContainer.clientWidth);
+		canvas.height = ~~(canvasContainer.clientHeight);
+		
+	}
+
+	window.onresize = scale;
+
+	scale();
+
+	var width = canvas.width;
+	var height = canvas.height;
+
+	var cells = new Uint8Array(height * width * 4);
+
+	for(var i = 0; i < cells.length; i+=4){
+		cells[i] = Math.round(Math.random())*255;
+	}
+
+	/*var middle = ~~(~~(height/2) * width + width/2);
+
+	for(var i = 0; i < 30; i++){
+		cells[middle+i] = 255;
+	}
+
+	cells[middle+width] = 255;*/
+
+	var renderer = new Renderer('renderCanvas');
+	renderer.render(cells);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
